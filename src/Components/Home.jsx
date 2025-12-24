5a9621c62a45167117deded93b553b1e9862507d
sumanth raju <sumanthraju4449@gmail.com>
2025-12-25 00:52:15 +0530

    Fix routing with BrowserRouter and Links

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,26 +1,27 @@
-import "../Style/Home.css";
 import React from "react";
+import { Link } from "react-router-dom";
+import "../Style/Home.css";
 
 const Home = () => {
   return (
-    <>
-      <div className="Home-headers">
-        <h1 className="Home-head1">Welcome to PopX</h1>
-        <p className="Home-para">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        </p>
-        <button className="create-account">
-          <a className="create-account" href="/register">
-            Create Account
-          </a>
-        </button>
-        <button className="login-link">
-          <a className="login-link" href="/login">
-            Already Registered? Login
-          </a>
-        </button>
+    <div className="Home-headers">
+      <h1 className="Home-head1">Welcome to PopX</h1>
+      <p className="Home-para">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      </p>
+
+      <div className="home-buttons">
+       
+        <Link to="/register">
+          <button className="create-account">Create Account</button>
+        </Link>
+
+        
+        <Link to="/login">
+          <button className="login-link">Already Registered? Login</button>
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
